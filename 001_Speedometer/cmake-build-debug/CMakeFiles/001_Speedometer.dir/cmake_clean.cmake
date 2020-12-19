5087498521d33fa01801500ddf6eedd544dbbc1c
Alex <alex0692pash@mail.ru>
2020-12-19 19:37:39 +0300

    Init 001

---
+++ 001_Speedometer/cmake-build-debug/CMakeFiles/001_Speedometer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "001_Speedometer.exe"
+  "001_Speedometer.exe.manifest"
+  "001_Speedometer.pdb"
+  "CMakeFiles/001_Speedometer.dir/main.cpp.obj"
+  "lib001_Speedometer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/001_Speedometer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

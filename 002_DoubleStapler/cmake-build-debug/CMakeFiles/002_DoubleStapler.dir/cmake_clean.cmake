429ffa5744a38c27fc2cffdf87d1f980cdc7235c
Alex <alex0692pash@mail.ru>
2020-12-21 23:36:16 +0300

    Init 002

---
+++ 002_DoubleStapler/cmake-build-debug/CMakeFiles/002_DoubleStapler.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "002_DoubleStapler.exe"
+  "002_DoubleStapler.exe.manifest"
+  "002_DoubleStapler.pdb"
+  "CMakeFiles/002_DoubleStapler.dir/main.cpp.obj"
+  "lib002_DoubleStapler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/002_DoubleStapler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 002_DoubleStapler/cmake-build-debug/CMakeFiles/002_DoubleStapler.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 002_DoubleStapler.
+# This may be replaced when dependencies are built.

5da8ac8d5b8adeafa862917e5335fb4b72c3753a
Alex <alex0692pash@mail.ru>
2020-12-22 21:14:55 +0300

    Init 003

---
+++ 003_Calculator/cmake-build-debug/CMakeFiles/003_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "003_Calculator.exe"
+  "003_Calculator.exe.manifest"
+  "003_Calculator.pdb"
+  "CMakeFiles/003_Calculator.dir/main.cpp.obj"
+  "lib003_Calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/003_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

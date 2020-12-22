e0d5c73a69f2f86cc0a0205ab8d6d7676e226314
Alex <alex0692pash@mail.ru>
2020-12-22 21:57:36 +0300

    Init 004

---
+++ 004_MechanicPiano/cmake-build-debug/CMakeFiles/004_MechanicPiano.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "004_MechanicPiano.exe"
+  "004_MechanicPiano.exe.manifest"
+  "004_MechanicPiano.pdb"
+  "CMakeFiles/004_MechanicPiano.dir/main.cpp.obj"
+  "lib004_MechanicPiano.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/004_MechanicPiano.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 004_MechanicPiano/cmake-build-debug/CMakeFiles/004_MechanicPiano.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 004_MechanicPiano.
+# This may be replaced when dependencies are built.

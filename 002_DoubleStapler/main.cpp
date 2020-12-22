e531c23a61e271f8f0d8a40a2a7498aab3b235bf
Alex <alex0692pash@mail.ru>
2020-12-22 21:09:32 +0300

    002 done

--- 002_DoubleStapler/main.cpp
+++ 002_DoubleStapler/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <sstream>
 #include <cmath>
+#include <string>
 
 /*
  * Надо написать небольшую программу.
@@ -9,39 +9,57 @@
  * Целая часть - это та часть числа, которая находится до точки (запятой), дробная - после.
  */
 
-//TO DO: Нет перевода 001 в .001!! Возможно, переделать на string to double
-
 bool Check(int number) {
     return number > 0;
 }
 
-double Divider(int number) {
+int CountZero(std::string str) {
     int count = 0;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == '0') {
+            count++;
+        } else {
+            break;
+        }
+    }
+    return count;
+}
+
+int CountSymbol(int number, int count) {
     while (number >= 1) {
         number /= 10;
         count++;
     }
-    return pow(10, count);
+    return count;
 }
 
 int main() {
-    std::stringstream text;
-    int integer;
-    int fraction;
+    std::string strInteger;
+    std::string strFraction;
+
     std::cout << "Input integer: ";
-    std::cin >> integer;
+    std::cin >> strInteger;
+    int integer = std::stoi(strInteger);
+
     std::cout << "Input fraction: ";
-    std::cin >> fraction;
+    std::cin >> strFraction;
+    int countDivider = CountZero(strFraction);
+    int fraction = std::stoi(strFraction);
+    countDivider = CountSymbol(fraction, countDivider);
+
     while (!Check(fraction)) {
         std::cout << "Incorrect value. Try again: ";
-        std::cin >> fraction;
+        std::cin >> strFraction;
+        countDivider = CountZero(strFraction);
+        fraction = std::stoi(strFraction);
+        countDivider = CountSymbol(fraction, countDivider);
     }
 
     double result = integer;
     if (result >= 0) {
-        result += fraction / Divider(fraction);
+        result += fraction / pow(10, countDivider);
     } else {
-        result -= fraction / Divider(fraction);
+        result -= fraction / pow(10, countDivider);
     }
     std::cout << result;
 }

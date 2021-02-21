a3f09058efeee88de8676b7b183b13a97de30836
Alex <alex0692pash@mail.ru>
2021-02-21 18:05:36 +0300

    005 init

---
+++ 005_SmartHouse/cmake-build-debug/CMakeFiles/005_SmartHouse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "005_SmartHouse.exe"
+  "005_SmartHouse.exe.manifest"
+  "005_SmartHouse.pdb"
+  "CMakeFiles/005_SmartHouse.dir/main.cpp.obj"
+  "lib005_SmartHouse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/005_SmartHouse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
